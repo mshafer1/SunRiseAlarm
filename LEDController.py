cd84707b14b7c25513a496b9740f2bcc8c83056d
Matthew <2565361+mshafer1@users.noreply.github.com>
2018-10-29 19:20:06 -0500

    Implementing LEDController as single instance to keep track of LED

--- LEDController.py
+++ LEDController.py
@@ -1,4 +1,5 @@
 import platform
+import config
 
 value = platform.platform()
 
@@ -6,6 +7,26 @@ if 'Windows' in value:
     # this is a no-op class for testing on Windows platforms
     class PWMLED():
         def __init__(self, pin):
-            pass
+            self.value = 0
 else:
     from gpiozero import PWMLED
+
+
+class LEDController(object):
+    def __init__(self):
+        self._led = PWMLED(config.LED_pin)
+        self._led.value = 0
+
+    def setValue(self, value):
+        self._led.value = value
+
+    def getValue(self):
+        return self._led.value
+
+    @property
+    def percentage(self):
+        return self._led.value * 100 // 1  # multiply by 100 and truncate
+
+    @percentage.setter
+    def percentage(self, value):
+        self._led.value = int(value) / 100
