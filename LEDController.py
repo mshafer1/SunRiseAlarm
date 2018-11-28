a5405fec38716bd89c31a3fb273985558d4f6ce3
Matthew <2565361+mshafer1@users.noreply.github.com>
2018-11-27 20:57:47 -0600

    swapping to pigpio for better pwm
    it turns out that most GPIO libraries use software timing for PWM pulses (resulting in some flashing), switching to pigpio which uses hardware timing to resolve this.

--- LEDController.py
+++ LEDController.py
@@ -7,33 +7,30 @@ value = platform.platform()
 
 if 'Windows' in value:
     # this is a no-op class for testing on Windows platforms
-    PWMLED = utilities._MockPWM_LED
+    GPIOLib = utilities._MockController
 
 else:
     #from gpiozero import PWMLED
-    import pigpio
+    import pigpio as GPIOLib
 
 
 class LEDController(object):
     def __init__(self):
-        self._pi = pigpio.pi()
-        #self._led = PWMLED(config.LED_pin)
-        #self._led.value = 0
-        #self._led.frequency = 1000
+        self._pi = GPIOLib.pi()
+        self._pin = config.LED_pin
+        self.value_raw = 0
 
     def __del__(self):
         self.value_raw = 0
         self._pi.stop()
-        #self._led.close()
-        #self._led = None
 
     @property
     def value_raw(self):
-        return self._pi.read(config.LED_pin)
+        return self._pi.read(self._pin)
 
     @value_raw.setter
     def value_raw(self, value):
-        self._pi.set_PWM_dutycycle(config.LED_pin, value)
+        self._pi.set_PWM_dutycycle(self._pin, value)
 
     @property
     def value(self):
@@ -46,15 +43,9 @@ class LEDController(object):
     @classmethod
     def _scale(cls, percent):
         # this is a scaling based on desired percentage to 8 bit power for linear brightness
-        eight_bit_val = (.0127 * percent ** 2 + 1.3027 * percent ) // 1.0089
+        scaled = .0127 * (percent ** 2) + 1.3027 * percent - 2
+        scaled = scaled // 1
 
-        # scale to [0,1]
         scaled = max(0, scaled) # remove negatives
         scaled = min(255, scaled) # remove high
         return scaled
-
-    @classmethod
-    def _inverse_scale(cls, raw):
-
-        result = (math.sqrt(1.307e9 * raw + 169.702e6) -13.027e3) / 254
-        return result
