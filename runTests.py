63b4fa03bab2b08cb60a05c44a4c7c20f3fc8886
Matthew Shafer (laptop) <mshafer1@harding.edu>
2019-07-30 19:17:48 -0500

    add coverage flag

--- runTests.py
+++ runTests.py
@@ -10,9 +10,14 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-V', dest="verbosity", action='count', default=0, help="Increase the level of logging output")
+    parser.add_argument('--cov', default=None, help='--cov argument to pass to pytest')
     args = parser.parse_args()
     verbosity = ['-v'] * args.verbosity
 
     pytest_args = [__file__, '-l'] + verbosity
+
+    if args.cov:
+        pytest_args.append('--cov')
+
     result = pytest.main(pytest_args)
     sys.exit(result)
