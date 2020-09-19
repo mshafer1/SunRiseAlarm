ab63f0add5a9c3c7411f10b2ebe084102bdfb708
Matthew <2565361+mshafer1@users.noreply.github.com>
2020-09-19 15:52:05 -0500

    parametrize target_datetime tests

--- __tests__/test_alarm.py
+++ __tests__/test_alarm.py
@@ -62,68 +62,23 @@ def test__mock_date__next_day__returns_expected_day(mock_datetime, set_days, exp
 
     assert expected_day == next_day
 
+# NOTE: Jan 1, 2006 is used because it is a sunday - yielding easy translation from day of Jan to day of week.
+@pytest.mark.parametrize("mock_datetime, set_days, expected_target", (
+    (datetime(2006, 1, 1, 5, 59), Days.SUNDAY, datetime(2006, 1, 1, 6, 0)), # one minute away
+    (datetime(2006, 1, 1, 6, 1), Days.SUNDAY, datetime(2006, 1, 8, 6, 0)), # minute after - give me next week
+    (datetime(2006, 1, 1, 6, 1), Days.SUNDAY + Days.MONDAY, datetime(2006, 1, 2, 6, 0)), # minute after - give me tomorrow
+    (datetime(2006, 1, 3, 5, 0), Days.SUNDAY, datetime(2006, 1, 8, 6, 0)), # sever days later - give me next week
+))
+def test__mock_date__taget_datetime__returns_expected_value(mock_datetime, set_days, expected_target, freezer):
+    freezer.move_to(mock_datetime)
+    alarm = Alarm()
+    alarm.add_target_day(set_days)
+    alarm.target_hour = 6
+    alarm.target_minute = 0
 
-class TestAlarmGetTargetDateTime(unittest.TestCase):
-    def test_today_times(self):
-        from datetime import datetime
-        with patch('utilities.TestableDateTime') as mock_date:
-            mock_date.now.return_value = datetime(2006, 1, 1, 5, 29)  # 5:29, Sunday, Jan 1st, 2006
-            alarm = Alarm()
-            alarm.add_target_day(Days.SUNDAY)
-            alarm.target_hour = 6
-            alarm.target_minute = 0
+    target = alarm.target_datetime
 
-            self.assertEqual(alarm.next_day, Days.SUNDAY)
-            target = alarm.target_datetime
-
-            self.assertEqual(target.hour, alarm.target_hour)
-            self.assertEqual(target.minute, alarm.target_minute)
-            now = utilities.TestableDateTime.now()
-            self.assertEqual(target.day, now.day)
-
-    def test_today_passed_times(self):
-        from datetime import datetime
-        with patch('utilities.TestableDateTime') as mock_date:
-            mock_date.now.return_value = datetime(2006, 1, 1, 6, 1)  # 6:01, Sunday, Jan 1st, 2006
-            alarm = Alarm()
-            alarm.add_target_day(Days.SUNDAY)
-            alarm.target_hour = 6
-            alarm.target_minute = 0
-
-            target = alarm.target_datetime
-            self.assertEqual(target.hour, alarm.target_hour)
-            self.assertEqual(target.minute, alarm.target_minute)
-            now = utilities.TestableDateTime.now()
-            self.assertEqual(target.day, now.day + 7)  # a week from now
-
-    def test_tomorrow_times(self):
-        from datetime import datetime
-        with patch('utilities.TestableDateTime') as mock_date:
-            mock_date.now.return_value = datetime(2006, 1, 1, 6, 1)  # 6:01, Sunday, Jan 1st, 2006
-            alarm = Alarm()
-            alarm.add_target_day(Days.MONDAY)
-            alarm.target_hour = 6
-            alarm.target_minute = 0
-
-            target = alarm.target_datetime
-            self.assertEqual(target.hour, alarm.target_hour)
-            self.assertEqual(target.minute, alarm.target_minute)
-            now = utilities.TestableDateTime.now()
-            self.assertEqual(target.day, now.day + 1) # tomorrow
-
-    def test_next_week_times(self):
-        from datetime import datetime
-        with patch('utilities.TestableDateTime') as mock_date:
-            mock_date.now.return_value = datetime(2006, 1, 3, 0, 0)  # midnight, Tuesday, Jan 3rd, 2006
-            alarm = Alarm()
-            alarm.add_target_day(Days.SUNDAY)
-            alarm.target_hour = 6
-            alarm.target_minute = 0
-
-            target = alarm.target_datetime
-            self.assertEqual(target.hour, alarm.target_hour)
-            self.assertEqual(target.minute, alarm.target_minute)
-            self.assertEqual(target.day, 8)  # Sunday the 8th
+    assert expected_target == target
 
 
 class TestGradualFadeUpOnTime(unittest.TestCase):
@@ -230,20 +185,29 @@ class TestGradualFadeUpOnTime(unittest.TestCase):
             self.assertEqual(0, alarm.get_desired_brightness())
 
 
-class TestAlarmDisabledStaysOff(unittest.TestCase):
-    def test_AlarmThatIsDisabledStaysAtValueZero(self):
-        with patch('utilities.TestableDateTime') as mock_date:
-            mock_date.now.return_value = datetime(2006, 1, 1, 6, 0)  # 6:00, Sunday, Jan 1st, 2006
-            alarm = Alarm()
-            alarm.add_target_day(Days.SUNDAY)
-            alarm.target_hour = 6
-            alarm.target_minute = 0
-            alarm.active = False
+def test__disabled_alarm__stays_off(freezer):
+    freezer.move_to(datetime(2006, 1, 1, 6, 0))
+    alarm = Alarm()
+    alarm.add_target_day(Days.SUNDAY)
+    alarm.target_hour = 6
+    alarm.target_minute = 0
+    alarm.active = False
 
-            self.assertEqual(0, alarm.get_desired_brightness())
+    brighness = alarm.get_desired_brightness()
 
-            alarm.active = True
-            self.assertEqual(100, alarm.get_desired_brightness())
+    assert brighness == 0
+
+def test__disabled_alarm__stays_off(freezer):
+    freezer.move_to(datetime(2006, 1, 1, 6, 0))
+    alarm = Alarm()
+    alarm.add_target_day(Days.SUNDAY)
+    alarm.target_hour = 6
+    alarm.target_minute = 0
+    alarm.active = False
+
+    brighness = alarm.get_desired_brightness()
+
+    assert brighness == 0
 
 
 if __name__ == '__main__':
